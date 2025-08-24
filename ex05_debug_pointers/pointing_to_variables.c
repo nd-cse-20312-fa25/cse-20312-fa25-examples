fec59639a0ca05baf26865cc7c66db712a8a11e4
Jay Brockman <jbb@nd.edu>
2025-08-24 14:31:27 -0400

    Changed some examples

--- ex05_debug_pointers/pointing_to_variables.c
+++ ex05_debug_pointers/pointing_to_variables.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int *p;
-    int *q;
-    int x = 5;
+    int x = 5;                  // x, y are ints
     int y = 1;
+    int *p;                     // p, q are addresses of ints
+    int *q;
 
-    p = &x; // p gets pointer to x
-    y = *p; // y gets value pointed to by p
+    p = &x;                     // p gets address of x
+    y = *p;                     // y gets value at address in p
     printf("y = %d\n", y);
 
-    q = p;  // q gets same pointer as p
+    q = p;                      // q gets same address as p
 
     printf("*p = %d    *q = %d\n", *p, *q); 
 }
